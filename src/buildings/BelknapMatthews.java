79d2369dda143ed0dd44e6fb6357f8f8580799af
Michael Diamond <dimo414@gmail.com>
2010-11-29 22:08:01 -0800

    Added Belknap/Matthews, changed move speed, and added another feetToGL() method in Util

---
+++ src/buildings/BelknapMatthews.java
@@ -0,0 +1,69 @@
+package buildings;
+
+import javax.media.opengl.GL;
+
+import com.sun.opengl.util.GLUT;
+
+import util.Util;
+
+/**
+ * <h3>Belknap, Matthews, Tera, and Beta Theta Pi</h3>
+ * 
+ * <h4>Identifying Location - Southwest Corner</h4>
+ * <p><strong>7547273.609E 472892.399N</strong></p>
+ *  
+ * @author Michael Diamond
+ */
+public class BelknapMatthews extends Building {
+	private double posEast = 7547273.609;
+	private double posNorth = 472892.399;
+	private double posElevation = 0; // TODO Get Elevation of building
+	private double[] glPos = Util.coordToGL(posEast, posNorth, posElevation);
+	private double[] midpoint = new double[]{Util.f(207,2)/2,Util.f(39+155+31,2+8)/2};
+	private GLUT glut = new GLUT();
+	
+	@Override
+	public void init(GL gl) {
+	}
+
+	@Override
+	public void draw(GL gl) {//TODO find a better way to position the building at the origin or not
+		gl.glPushMatrix();
+		
+		// Universal positioning
+		if(!drawOrigin){
+			gl.glTranslated(glPos[0],glPos[1],glPos[2]);
+			gl.glRotated(buildingRotation, 0, 1, 0);
+		}
+		else
+			// this is the appx centerpoint of the building
+			gl.glTranslated(-midpoint[0], 0, midpoint[1]);
+		// End universal positioning
+		
+		// belknap
+		gl.glPushMatrix();
+		gl.glScaled(Util.feetToGL(39,2), Util.feetToGL(30), Util.feetToGL(207,2));
+		gl.glTranslated(.5,.5,-.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		// matthews
+		gl.glPushMatrix();
+		gl.glTranslated(Util.feetToGL(39,2),0,-Util.feetToGL(59.8));
+		gl.glScaled(Util.feetToGL(155), Util.feetToGL(40), Util.feetToGL(95,4));
+		gl.glTranslated(.5,.5,-.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		// tera/beta
+		gl.glPushMatrix();
+		gl.glTranslated(Util.feetToGL(39+155,2),0,0);
+		gl.glScaled(Util.feetToGL(31,8), Util.feetToGL(30), Util.feetToGL(207,2));
+		gl.glTranslated(.5,.5,-.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+				
+		gl.glPopMatrix();
+	}
+
+}
