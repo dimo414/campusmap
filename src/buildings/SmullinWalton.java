d301c3c2f81c6b29457819fe4f42bf2344f79fec
Brian Forbis <bforbis@willamette.edu>
2010-11-30 13:58:29 -0800

    Smullin Walton done

--- src/buildings/SmullinWalton.java
+++ src/buildings/SmullinWalton.java
@@ -2,37 +2,90 @@ package buildings;
 
 import javax.media.opengl.GL;
 
+import util.Util;
+
+import com.sun.opengl.util.GLUT;
+
 /**
- * Smullin / Walton Hall
- *
- * THIS JAVADOC OR SOME OTHER COMMENT SHOULD CONTAIN DETAILS LIKE POSITION AND DIMENSIONS OF THE BUILDING
+ * <h3>Smullin / Walton Hall</h3>
+ * 
+ * <h4>Identifying Location - SW Corner</h4>
+ * <p><strong>SOMETHING-E SOMETHING-N</strong></p>
+
+ * 
+ * @author Brian Forbis
  */
 public class SmullinWalton extends Building{
-	double length = 167 + 4. / 12;
-	double width = 95 + 2. / 12;
-	double height;
-
-	double[][] vertices = { { 0, 0, 0 }, { 0, 0, width }, { length, 0, width },
-			{ length, 0, 0 }, { 0, height, 0 }, { 0, height, width },
-			{ length, height, width }, { length, height, 0 } };
-	int[][] faces = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-			{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																// East, North,
-																// Bottom, Top
-	double[][] normals = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-			{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-	double[] position = { 0, 0, 0 };
-	double textures[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
+	private double posEast = 7547193.302; // TODO Find position East
+	private double posNorth = 473208.16; // TODO Find position North
+	private double posElevation = 0; // TODO Get Elevation of building
+	private double[] glPos;
+	private GLUT glut = new GLUT();
+	
+	private double height = 20;
 	@Override
 	public void draw(GL gl) {
-		// TODO Auto-generated method stub
+		gl.glPushMatrix();
 		
+		// Universal positioning
+		if(!drawOrigin){
+			gl.glTranslated(glPos[0],glPos[1],glPos[2]);
+			gl.glRotated(buildingRotation, 0, 1, 0);
+		}
+		else //TODO Find Centerpoint
+			// this is the appx centerpoint of the building
+			gl.glTranslated(-Util.feetToGL(30), 0, Util.feetToGL(30));
+		// End universal positioning
+		
+		//Start Drawing, SouthWest Positions
+		
+		gl.glPushMatrix(); //South extrusion
+		gl.glTranslated(Util.feetToGL(37+3./12), 0, Util.feetToGL(8+9./12));
+		gl.glScaled(Util.feetToGL(65+1./12), Util.feetToGL(height), Util.feetToGL(8+9./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPushMatrix(); //Walton Hall
+		gl.glScaled(Util.feetToGL(139+7./12), Util.feetToGL(height), Util.feetToGL(36+7./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPushMatrix(); //Middle Section
+		gl.glTranslated(Util.feetToGL(29+11./12), 0, -Util.feetToGL(36+7./12));
+		gl.glScaled(Util.feetToGL(59+4./12), Util.feetToGL(height), Util.feetToGL(62+2./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPushMatrix(); //Bike Shed
+		gl.glTranslated(Util.feetToGL(89+3./12), 0, -Util.feetToGL(84+7./12));
+		gl.glScaled(Util.feetToGL(21+2./12), Util.feetToGL(height), Util.feetToGL(14+2./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPushMatrix(); //Smullin Hall
+		gl.glTranslated(-Util.feetToGL(9+8./12), 0, -Util.feetToGL(98+9./12));
+		gl.glScaled(Util.feetToGL(166), Util.feetToGL(height), Util.feetToGL(35+6./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPushMatrix(); //North Extrusion
+		gl.glTranslated(Util.feetToGL(26+2./12), 0, -Util.feetToGL(134+3./12));
+		gl.glScaled(Util.feetToGL(94+4./12), Util.feetToGL(height), Util.feetToGL(18+6./12));
+		gl.glTranslated(.5, .5, -.5);
+		glut.glutSolidCube(1);
+		gl.glPopMatrix();
+		
+		gl.glPopMatrix();
 	}
 
 	@Override
 	public void init(GL gl) {
-		// TODO Auto-generated method stub
-		
+		glPos = Util.coordToGL(posEast, posNorth, posElevation);		
 	}
 
 }
