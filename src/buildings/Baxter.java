1aad7ed739c65db5e909972dd442f89659f29d12
Jose Alvarado <josealvarado1@yahoo.com>
2010-12-04 14:21:29 -0800

    switched to Shape objects

--- src/buildings/Baxter.java
+++ src/buildings/Baxter.java
@@ -2,6 +2,7 @@ package buildings;
 
 import javax.media.opengl.GL;
 
+import util.Shape;
 import util.Util;
 
 /**
@@ -12,64 +13,17 @@ import util.Util;
 public class Baxter extends Building{
 
 	//1 - bottom left cube of baxter, 2 - middle cube of baxter, 3 - right cube of baxter
-//--------------------------------------------------------------------------------------------------	
-	double length1 = 66; 	//66' - 0       
-	double width1 = 36; 	//36' - 1 3/4
-	double height1 = 50;
-
-	double[][] vertices1 = { { 0, 0, 0 }, { 0, 0, width1 }, { length1, 0, width1 },
-			{ length1, 0, 0 }, { 0, height1, 0 }, { 0, height1, width1 },
-			{ length1, height1, width1 }, { length1, height1, 0 } };
-	int[][] faces1 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-			{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																// East, North,
-																// Bottom, Top
-	double[][] normals1 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-			{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-	double[] position1 = { 0, 0, 0 };
-	double textures1[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-//----------------------------------------------------------------------------------------------------
-	double length2 = 134 - 66;		//134' - 8 5/16     -   66'
-	double width2 = 202;		//202' - 10 1/8
-	double height2 = 50;
 	
-	double[][] vertices2 = { { 0, 0, 0 }, { 0, 0, width2 }, { length2, 0, width2 },
-			{ length2, 0, 0 }, { 0, height2, 0 }, { 0, height2, width2 },
-			{ length2, height2, width2 }, { length2, height2, 0 } };
-	int[][] faces2 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-			{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																// East, North,
-																// Bottom, Top
-	double[][] normals2 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-			{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-	double[] position2 = { 134 - 66, 0, 0 };
-	double textures2[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-//----------------------------------------------------------------------------------------------------	
-	double length3 = 205;		//205' - 0 9/16
-	double width3 = 36;		//36' - 4
-	double height3 = 50;
-	
-	double[][] vertices3 = { { 0, 0, 0 }, { 0, 0, width3 }, { length3, 0, width3 },
-			{ length3, 0, 0 }, { 0, height3, 0 }, { 0, height3, width3 },
-			{ length3, height3, width3 }, { length3, height3, 0 } };
-	int[][] faces3 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-			{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																// East, North,
-																// Bottom, Top
-	double[][] normals3 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-			{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-	double[] position3 = { 0, 0, 202 };
-	double textures3[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-	
-	private double posEast = 7547348.62;
-	private double posNorth = 473129.605;
+	double posEast = 7547580.116;
+	private double posNorth = 473020.902;
 	private double posElevation = 0; // TODO Get Elevation of building
 	private double[] glPos = Util.coordToGL(posEast, posNorth, posElevation);
+	private double[] midpoint = new double[]{Util.feetToGL(205/2),Util.feetToGL(238/2)};
 	
 	@Override
 	public void draw(GL gl) {
 		gl.glPushMatrix();
-		
+	
 		// Universal positioning
 		if(!drawOrigin){
 			gl.glTranslated(glPos[0],glPos[1],glPos[2]);
@@ -77,61 +31,35 @@ public class Baxter extends Building{
 		}
 		else
 			// this is the appx centerpoint of the building
-			gl.glTranslated(-Util.feetToGL(205.0/2), 0, Util.feetToGL(238.0/2));
+			gl.glTranslated(-midpoint[0], 0, midpoint[1]);
 		// End universal positioning
-		
 		
 		gl.glRotated(90, 0, 1, 0);
 		
+		// 1st Cube
 		gl.glPushMatrix();
-		gl.glTranslated(position1[0], position1[1], position1[2]);
-		for (int i = 0; i < faces1.length; i++) {
-			// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-			gl.glBegin(GL.GL_QUADS);
-			for (int j = 0; j < 4; j++) {
-				gl.glNormal3dv(normals1[i], 0);
-				gl.glTexCoord2dv(textures1[j], 0);
-				gl.glVertex3dv(vertices1[faces1[i][j]], 0);
-			}
-			gl.glEnd();
-		}
+		gl.glScaled(Util.feetToGL(205), Util.feetToGL(50), Util.feetToGL(36));
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
-		//-----------------------------------------------------------------
+		
+		// 2nd Cube
 		gl.glPushMatrix();
-		gl.glTranslated(position2[0], position2[1], position2[2]);
-		for (int i = 0; i < faces2.length; i++) {
-			// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-			gl.glBegin(GL.GL_QUADS);
-			for (int j = 0; j < 4; j++) {
-				gl.glNormal3dv(normals2[i], 0);
-				gl.glTexCoord2dv(textures2[j], 0);
-				gl.glVertex3dv(vertices2[faces2[i][j]], 0);
-			}
-			gl.glEnd();
-		}
+		gl.glTranslated(Util.feetToGL(66),0,-Util.feetToGL(36));
+		gl.glScaled(Util.feetToGL(134-66), Util.feetToGL(50), Util.feetToGL(202));
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
-		//-----------------------------------------------------------------
+		
+		// 3rd Cube
 		gl.glPushMatrix();
-		gl.glTranslated(position3[0], position3[1], position3[2]);
-		for (int i = 0; i < faces3.length; i++) {
-			// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-			gl.glBegin(GL.GL_QUADS);
-			for (int j = 0; j < 4; j++) {
-				gl.glNormal3dv(normals3[i], 0);
-				gl.glTexCoord2dv(textures3[j], 0);
-				gl.glVertex3dv(vertices3[faces3[i][j]], 0);
-			}
-			gl.glEnd();
-		}
-		gl.glPopMatrix();	
-		
-		
+		gl.glTranslated(Util.feetToGL(0),0,-Util.feetToGL(202));
+		gl.glScaled(Util.feetToGL(66), Util.feetToGL(50), Util.feetToGL(36));
+		Shape.Cube.draw(gl);
+		gl.glPopMatrix();
 		
 		gl.glPopMatrix();
 	}
 
 	@Override
 	public void init(GL gl) {
-		glPos = Util.coordToGL(posEast, posNorth, posElevation);
 	}
 }
--- src/buildings/WestSide.java
+++ src/buildings/WestSide.java
@@ -2,6 +2,7 @@ package buildings;
 
 import javax.media.opengl.GL;
 
+import util.Shape;
 import util.Util;
 
 /**
@@ -12,59 +13,12 @@ import util.Util;
 public class WestSide extends Building{
 
 	//1 - Laussane, 2 - Campus Safety, 3 - right cube of Doney
-	//--------------------------------------------------------------------------------------------------	
-		double length1 = 184;      
-		double width1 = 61;
-		double height1 = 50;
 
-		double[][] vertices1 = { { 0, 0, 0 }, { 0, 0, width1 }, { length1, 0, width1 },
-				{ length1, 0, 0 }, { 0, height1, 0 }, { 0, height1, width1 },
-				{ length1, height1, width1 }, { length1, height1, 0 } };
-		int[][] faces1 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals1 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position1 = { 0, 0, 0 };
-		double textures1[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-	//----------------------------------------------------------------------------------------------------
-		double length2 = 59;
-		double width2 = 76;
-		double height2 = 50;
-		
-		double[][] vertices2 = { { 0, 0, 0 }, { 0, 0, width2 }, { length2, 0, width2 },
-				{ length2, 0, 0 }, { 0, height2, 0 }, { 0, height2, width2 },
-				{ length2, height2, width2 }, { length2, height2, 0 } };
-		int[][] faces2 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals2 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position2 = { 125, 0, 61};
-		double textures2[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-		//----------------------------------------------------------------------------------------------------
-		double length3 = 161;	
-		double width3 = 40;
-		double height3 = 50;
-		
-		double[][] vertices3 = { { 0, 0, 0 }, { 0, 0, width3 }, { length3, 0, width3 },
-				{ length1, 0, 0 }, { 0, height1, 0 }, { 0, height1, width3 },
-				{ length3, height3, width3 }, { length3, height3, 0 } };
-		int[][] faces3 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals3 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position3 = { 23, 0, 137};
-		double textures3[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-		
-		private double posEast = 7546390.251;
-		private double posNorth = 473271.586;
+		private double posEast = 7546544.602;
+		private double posNorth = 473207.586;
 		private double posElevation = 0; // TODO Get Elevation of building
 		private double[] glPos = Util.coordToGL(posEast, posNorth, posElevation);
+		private double[] midpoint = new double[]{Util.feetToGL(205/2),Util.feetToGL(238/2)};
 		
 		@Override
 		public void draw(GL gl) {
@@ -77,60 +31,36 @@ public class WestSide extends Building{
 			}
 			else
 				// this is the appx centerpoint of the building
-				gl.glTranslated(-Util.feetToGL(184.0/2), 0, Util.feetToGL(177.0/2));
+				gl.glTranslated(-midpoint[0], 0, midpoint[1]);
 			// End universal positioning
 			
 			gl.glRotated(90, 0, 1, 0);		
 			
+			// 1st Cube
 			gl.glPushMatrix();
-			gl.glTranslated(position1[0], position1[1], position1[2]);
-			for (int i = 0; i < faces1.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals1[i], 0);
-					gl.glTexCoord2dv(textures1[j], 0);
-					gl.glVertex3dv(vertices1[faces1[i][j]], 0);
-				}
-				gl.glEnd();
-			}
+			gl.glScaled(Util.feetToGL(161), Util.feetToGL(50), Util.feetToGL(40));
+			Shape.Cube.draw(gl);
 			gl.glPopMatrix();
-			//-----------------------------------------------------------------
-			gl.glPushMatrix();
-			gl.glTranslated(position2[0], position2[1], position2[2]);
-			for (int i = 0; i < faces2.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals2[i], 0);
-					gl.glTexCoord2dv(textures2[j], 0);
-					gl.glVertex3dv(vertices2[faces2[i][j]], 0);
-				}
-				gl.glEnd();
-			}
-			gl.glPopMatrix();	
-			//-----------------------------------------------------------------
-			gl.glPushMatrix();
-			gl.glTranslated(position3[0], position3[1], position3[2]);
-			for (int i = 0; i < faces3.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals3[i], 0);
-					gl.glTexCoord2dv(textures3[j], 0);
-					gl.glVertex3dv(vertices3[faces3[i][j]], 0);
-				}
-				gl.glEnd();
-			}
-			gl.glPopMatrix();	
 			
+			// 2nd Cube
+			gl.glPushMatrix();
+			gl.glTranslated(Util.feetToGL(161-59),0,-Util.feetToGL(40));
+			gl.glScaled(Util.feetToGL(59), Util.feetToGL(50), Util.feetToGL(76));
+			Shape.Cube.draw(gl);
+			gl.glPopMatrix();
+			
+			// 3rd Cube
+			gl.glPushMatrix();
+			gl.glTranslated(-Util.feetToGL(184-161),0,-Util.feetToGL(40+76));
+			gl.glScaled(Util.feetToGL(184), Util.feetToGL(50), Util.feetToGL(61));
+			Shape.Cube.draw(gl);
+			gl.glPopMatrix();
 			
 			gl.glPopMatrix();
 		}
 
 		@Override
 		public void init(GL gl) {
-			glPos = Util.coordToGL(posEast, posNorth, posElevation);
 		}
 	
 }
