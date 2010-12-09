bbf2d46091ee0b1687b8c8f4bd718f25d245c381
Jose Alvarado <josealvarado1@yahoo.com>
2010-12-09 12:29:01 -0800

    Modified the remianing buildings to use shape

--- src/buildings/AlphaChi.java
+++ src/buildings/AlphaChi.java
@@ -2,6 +2,7 @@ package buildings;
 
 import javax.media.opengl.GL;
 
+import util.Shape;
 import util.Util;
 
 /**
@@ -11,72 +12,8 @@ import util.Util;
 public class AlphaChi extends Building{
 
 	//1 - left building, 2 - middle, 3 - right
-	//--------------------------------------------------------------------------------------------------	
-		double length1 = 21;      
-		double width1 = 121;
-		double height1 = 50;
 
-		double[][] vertices1 = { { 0, 0, 0 }, { 0, 0, width1 }, { length1, 0, width1 },
-				{ length1, 0, 0 }, { 0, height1, 0 }, { 0, height1, width1 },
-				{ length1, height1, width1 }, { length1, height1, 0 } };
-		int[][] faces1 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals1 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position1 = { 0, 0, 0 };
-		double textures1[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-	//----------------------------------------------------------------------------------------------------
-		double length2 = 65;
-		double width2 = 73;
-		double height2 = 50;
-		
-		double[][] vertices2 = { { 0, 0, 0 }, { 0, 0, width2 }, { length2, 0, width2 },
-				{ length2, 0, 0 }, { 0, height2, 0 }, { 0, height2, width2 },
-				{ length2, height2, width2 }, { length2, height2, 0 } };
-		int[][] faces2 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals2 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position2 = { 21, 0, 3};
-		double textures2[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-		//----------------------------------------------------------------------------------------------------
-		double length3 = 26;	
-		double width3 = 40;
-		double height3 = 50;
-		
-		double[][] vertices3 = { { 0, 0, 0 }, { 0, 0, width3 }, { length3, 0, width3 },
-				{ length1, 0, 0 }, { 0, height1, 0 }, { 0, height1, width3 },
-				{ length3, height3, width3 }, { length3, height3, 0 } };
-		int[][] faces3 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals3 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position3 = { 86, 0, -7};
-		double textures3[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-		//----------------------------------------------------------------------------------------------------
-		double length4 = 33;
-		double width4 = 21;
-		double height4 = 50;
-		
-		double[][] vertices4 = { { 0, 0, 0 }, { 0, 0, width4 }, { length4, 0, width4 },
-				{ length4, 0, 0 }, { 0, height4, 0 }, { 0, height4, width4 },
-				{ length4, height4, width4 }, { length4, height4, 0 } };
-		int[][] faces4 = { { 4, 0, 1, 5 }, { 5, 1, 2, 6 }, { 6, 2, 3, 7 },
-				{ 7, 3, 0, 4 }, { 0, 1, 2, 3 }, { 4, 5, 6, 7 } }; // West, South,
-																	// East, North,
-																	// Bottom, Top
-		double[][] normals4 = { { -1, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 },
-				{ 0, 0, -1 }, { 0, -1, 0 }, { 0, 1, 0 } };
-		double[] position4 = { 86, 0, 52};
-		double textures4[][] = {{0, 1}, {0,0}, {1,0}, {1,1}};
-
-		private double posEast = 7546451.18;
+		private double posEast = 7546481.18;
 		private double posNorth = 472500.315;
 		private double posElevation = 0; // TODO Get Elevation of building
 		private double[] glPos = Util.coordToGL(posEast, posNorth, posElevation);
@@ -97,61 +34,33 @@ public class AlphaChi extends Building{
 			
 			gl.glRotated(90, 0, 1, 0);		
 			
+			Shape.Cube.setColor(Building.brick);
+			// 1st Cube
 			gl.glPushMatrix();
-			gl.glTranslated(position1[0], position1[1], position1[2]);
-			for (int i = 0; i < faces1.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals1[i], 0);
-					gl.glTexCoord2dv(textures1[j], 0);
-					gl.glVertex3dv(vertices1[faces1[i][j]], 0);
-				}
-				gl.glEnd();
-			}
+			gl.glScaled(Util.feetToGL(21), Util.feetToGL(50), Util.feetToGL(121));
+			Shape.Cube.draw(gl);
 			gl.glPopMatrix();
-			//-----------------------------------------------------------------
+			
+			// 2nd Cube
 			gl.glPushMatrix();
-			gl.glTranslated(position2[0], position2[1], position2[2]);
-			for (int i = 0; i < faces2.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals2[i], 0);
-					gl.glTexCoord2dv(textures2[j], 0);
-					gl.glVertex3dv(vertices2[faces2[i][j]], 0);
-				}
-				gl.glEnd();
-			}
+			gl.glTranslated(Util.feetToGL(21),0,-Util.feetToGL(121-76));
+			gl.glScaled(Util.feetToGL(65), Util.feetToGL(50), Util.feetToGL(73));
+			Shape.Cube.draw(gl);
+			gl.glPopMatrix();
+			
+			// 3rd Cube
+			gl.glPushMatrix();
+			gl.glTranslated(Util.feetToGL(21+65),0,-Util.feetToGL(121-85));
+			gl.glScaled(Util.feetToGL(33), Util.feetToGL(50), Util.feetToGL(21));
+			Shape.Cube.draw(gl);
 			gl.glPopMatrix();	
-			//-----------------------------------------------------------------
+			
+			// 4th Cube
 			gl.glPushMatrix();
-			gl.glTranslated(position3[0], position3[1], position3[2]);
-			for (int i = 0; i < faces3.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals3[i], 0);
-					gl.glTexCoord2dv(textures3[j], 0);
-					gl.glVertex3dv(vertices3[faces3[i][j]], 0);
-				}
-				gl.glEnd();
-			}
-			gl.glPopMatrix();	
-			//-----------------------------------------------------------------
-			gl.glPushMatrix();
-			gl.glTranslated(position4[0], position4[1], position4[2]);
-			for (int i = 0; i < faces4.length; i++) {
-				// gl.glBindTexture(GL.GL_TEXTURE_2D, BUILDING_TEX);
-				gl.glBegin(GL.GL_QUADS);
-				for (int j = 0; j < 4; j++) {
-					gl.glNormal3dv(normals4[i], 0);
-					gl.glTexCoord2dv(textures4[j], 0);
-					gl.glVertex3dv(vertices4[faces4[i][j]], 0);
-				}
-				gl.glEnd();
-			}
-			gl.glPopMatrix();				
+			gl.glTranslated(Util.feetToGL(21+65),0,-Util.feetToGL(121-30));
+			gl.glScaled(Util.feetToGL(26), Util.feetToGL(50), Util.feetToGL(40));
+			Shape.Cube.draw(gl);
+			gl.glPopMatrix();
 			
 			gl.glPopMatrix();
 		}
