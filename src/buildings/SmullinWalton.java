93c353a1cfd2793521e5e50d01d4b77c443e99dd
Michael Diamond <dimo414@gmail.com>
2010-12-08 22:49:05 -0800

    Minor fixes to a couple of buildings.

--- src/buildings/SmullinWalton.java
+++ src/buildings/SmullinWalton.java
@@ -2,9 +2,8 @@ package buildings;
 
 import javax.media.opengl.GL;
 
+import util.Shape;
 import util.Util;
-
-import com.sun.opengl.util.GLUT;
 
 /**
  * <h3>Smullin / Walton Hall</h3>
@@ -20,7 +19,6 @@ public class SmullinWalton extends Building{
 	private double posNorth = 473205.738;
 	private double posElevation = 0; // TODO Get Elevation of building
 	private double[] glPos;
-	private GLUT glut = new GLUT();
 	
 	private double height = 20;
 	@Override
@@ -42,42 +40,36 @@ public class SmullinWalton extends Building{
 		gl.glPushMatrix(); //South extrusion
 		gl.glTranslated(Util.feetToGL(37+3./12), 0, Util.feetToGL(8+9./12));
 		gl.glScaled(Util.feetToGL(65+1./12), Util.feetToGL(height), Util.feetToGL(8+9./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPushMatrix(); //Walton Hall
 		gl.glScaled(Util.feetToGL(139+7./12), Util.feetToGL(height), Util.feetToGL(36+7./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPushMatrix(); //Middle Section
 		gl.glTranslated(Util.feetToGL(29+11./12), 0, -Util.feetToGL(36+7./12));
 		gl.glScaled(Util.feetToGL(59+4./12), Util.feetToGL(height), Util.feetToGL(62+2./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPushMatrix(); //Bike Shed
 		gl.glTranslated(Util.feetToGL(89+3./12), 0, -Util.feetToGL(84+7./12));
 		gl.glScaled(Util.feetToGL(21+2./12), Util.feetToGL(height), Util.feetToGL(14+2./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPushMatrix(); //Smullin Hall
 		gl.glTranslated(-Util.feetToGL(9+8./12), 0, -Util.feetToGL(98+9./12));
 		gl.glScaled(Util.feetToGL(166), Util.feetToGL(height), Util.feetToGL(35+6./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPushMatrix(); //North Extrusion
 		gl.glTranslated(Util.feetToGL(26+2./12), 0, -Util.feetToGL(134+3./12));
 		gl.glScaled(Util.feetToGL(94+4./12), Util.feetToGL(height), Util.feetToGL(18+6./12));
-		gl.glTranslated(.5, .5, -.5);
-		glut.glutSolidCube(1);
+		Shape.Cube.draw(gl);
 		gl.glPopMatrix();
 		
 		gl.glPopMatrix();
